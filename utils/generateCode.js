da927f32c63d091c78bfb7d0484e3ba7f87bd813
Nazar-Markiv-Mn <69341802+nazarMn@users.noreply.github.com>
2024-06-12 17:36:29 +0300

    Add files via upload

---
+++ utils/generateCode.js
@@ -0,0 +1,12 @@
+function generateCode(length = 6) {
+    const characters = '0123456789';
+    let code = '';
+
+    for (let i = 0; i < length; i++) {
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+
+}
+
+module.exports = generateCode;
